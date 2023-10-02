1a89fcd59c1f08e3fbd4ab68b609c9718329e932
Zy5879 <ZaireM21@gmail.com>
2023-10-02 18:08:42 -0400

    adding rock paper and scissors for user's and robot to signal that the game will be played against computer

--- rps-swift/ContentView.swift
+++ rps-swift/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        let rps: Array<String> = ["🪨", "📄", "✂️"]
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("🤖")
+                .font(.system(size: 60))
+            HStack {
+                ForEach(rps, id: \.self) { rps in
+                    Text(rps).font(.system(size: 60))
+                }
+                
+            }
+            .padding()
+            
         }
-        .padding()
     }
 }
 
