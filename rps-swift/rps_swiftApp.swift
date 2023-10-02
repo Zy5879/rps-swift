909a72671c4ea0e2016a55654fcab12e701bd79d
Zy5879 <ZaireM21@gmail.com>
2023-10-02 17:31:03 -0400

    Initial Commit

---
+++ rps-swift/rps_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  rps_swiftApp.swift
+//  rps-swift
+//
+//  Created by Zaire McAllister on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct rps_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
